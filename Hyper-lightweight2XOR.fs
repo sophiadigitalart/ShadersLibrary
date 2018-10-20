4143362c564819ad69f0519b3c0ed46308fd9af7
Bruce LANE <bruce@batchass.fr>
2018-10-20 17:06:27 +0200

    Hyper-lightweight2XOR

---
+++ Hyper-lightweight2XOR.fs
@@ -0,0 +1,49 @@
+/*{
+	"CREDIT" : "Hyper-lightweight2XOR by FabriceNeyret2",
+	"CATEGORIES" : [
+		"ci"
+	],
+	"DESCRIPTION": "",
+	"INPUTS": [
+		{
+			"NAME": "inputImage",
+			"TYPE" : "image"
+		},
+		{
+			"NAME": "iZoom",
+			"TYPE" : "float",
+			"MIN" : 0.0,
+			"MAX" : 1.0,
+			"DEFAULT" : 1.0
+		},
+		{
+			"NAME": "iSteps",
+			"TYPE" : "float",
+			"MIN" : 2.0,
+			"MAX" : 75.0,
+			"DEFAULT" : 19.0
+		},
+		{
+			"NAME" :"iMouse",
+			"TYPE" : "point2D",
+			"DEFAULT" : [0.0, 0.0],
+			"MAX" : [640.0, 480.0],
+			"MIN" : [0.0, 0.0]
+		},
+		{
+			"NAME": "iColor", 
+			"TYPE" : "color", 
+			"DEFAULT" : [
+				0.9, 
+				0.6, 
+				0.0, 
+				1.0
+			]
+		}
+	],
+}
+*/
+// https://www.shadertoy.com/view/4slXWn
+
+#define f(a,b)sin(50.3*length(gl_FragCoord.xy/RENDERSIZE.xy*4.-vec2(cos(a),sin(b))-3.))
+void main(){float t=TIME;gl_FragColor=vec4(f(t,t)*f(1.4*t,.7*t));}
