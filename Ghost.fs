c173e970c8a09315683286dbb2ac05cbf9a67317
brucelane <bruce@batchass.fr>
2018-11-22 20:15:26 +0100

    Ghost.fs Speed.fs

---
+++ Ghost.fs
@@ -0,0 +1,127 @@
+/*{
+	"CREDIT" : "Ghost by lsdlive",
+	"CATEGORIES" : [
+		"ci"
+	],
+	"DESCRIPTION": "",
+	"INPUTS": [
+		{
+			"NAME": "inputImage",
+			"TYPE" : "image"
+		},
+		{
+			"NAME": "iZoom",
+			"TYPE" : "float",
+			"MIN" : 0.0,
+			"MAX" : 1.0,
+			"DEFAULT" : 1.0
+		},
+		{
+			"NAME": "iSteps",
+			"TYPE" : "float",
+			"MIN" : 2.0,
+			"MAX" : 75.0,
+			"DEFAULT" : 19.0
+		},
+		{
+			"NAME" :"iMouse",
+			"TYPE" : "point2D",
+			"DEFAULT" : [0.0, 0.0],
+			"MAX" : [640.0, 480.0],
+			"MIN" : [0.0, 0.0]
+		},
+		{
+			"NAME": "iColor", 
+			"TYPE" : "color", 
+			"DEFAULT" : [
+				0.9, 
+				0.6, 
+				0.0, 
+				1.0
+			]
+		}
+	],
+}
+*/
+// https://www.shadertoy.com/view/MsVyRh
+
+vec2 path(float t) {
+    float a = sin(t*.2+1.5),b=sin(t*.2);
+    return vec2(a*2., a*b);
+}
+
+mat2 r2d(float a) {
+    float c=cos(a),s=sin(a);
+    return mat2(c, s, -s, c);
+}
+
+vec2 mo(vec2 p, vec2 d) {
+    p.x = abs(p.x) - d.x;
+    p.y = abs(p.y) - d.y;
+    if(p.y>p.x)p=p.yx;
+    return p;
+}
+
+float g=0.;
+float de(vec3 p) {
+   
+    vec3 q = p;
+    q.x += q.z*.1;
+    q.z += TIME*.1;
+    q = mod(q-1., 2.)-1.;
+    float s = length(q) - .001 + sin(TIME*30.)*.005;
+    
+    p.xy -= path(p.z);
+    
+    p.xy *= r2d(p.z*.9);
+    p.xy=mo(p.xy, vec2(.6, .12));
+    //mo(p.xy, vec2(.9, .2));
+    
+    p.xy *= r2d(p.z*.5);
+    
+    //mo(p.zy, vec2(.1, .2));
+    p.x = abs(p.x) - .4;
+    float d = length(p.xy) - .02 - (.5+.5*sin(p.z))*.05;
+    
+    d = min(d, s);
+    
+    
+    g+=.01/(.01+d*d);
+    return d;
+}
+
+void main(void)
+{  		
+		vec2 uv = (gl_FragCoord.xy/RENDERSIZE.xy -.5)*iZoom;
+    uv.x*=RENDERSIZE.x/RENDERSIZE.y;
+    
+    float dt = TIME * 6.;
+    vec3 ro = vec3(0,0, -3. + dt);
+    vec3 ta = vec3(0, 0, dt);
+    
+    ro.xy += path(ro.z);
+    ta.xy += path(ta.z);
+    
+    vec3 fwd = normalize(ta -ro);
+    vec3 left = cross(vec3(0,1,0),fwd);
+    vec3 up = cross(fwd, left);
+    
+    vec3 rd = normalize(fwd + left*uv.x+up*uv.y);
+
+    vec3 p;
+    float ri,t=0.;
+    for(float i=0.;i<1.;i+=.01) {
+    	ri = i;
+        p=ro+rd*t;
+        float d = de(p);
+        if(d<.001) break;
+        t+=d*.2;
+    }
+	vec3 bg =  vec3(.2, .6, .6)*.2; 
+    vec3 col = mix(vec3(.4, .5, .5), bg,ri);
+    col += g*.02;
+    
+    col = mix(col, bg, 1.-exp(-.01*t*t));
+
+    fragColor = vec4(col,1.0);
+}
---
+++ Speed.fs
@@ -0,0 +1,158 @@
+/*{
+	"CREDIT" : "Speed by lsdlive",
+	"CATEGORIES" : [
+		"ci"
+	],
+	"DESCRIPTION": "",
+	"INPUTS": [
+		{
+			"NAME": "inputImage",
+			"TYPE" : "image"
+		},
+		{
+			"NAME": "iZoom",
+			"TYPE" : "float",
+			"MIN" : -3.0,
+			"MAX" : 1.0,
+			"DEFAULT" : 1.0
+		},
+		{
+			"NAME": "iSteps",
+			"TYPE" : "float",
+			"MIN" : 2.0,
+			"MAX" : 75.0,
+			"DEFAULT" : 19.0
+		},
+		{
+			"NAME" :"iMouse",
+			"TYPE" : "point2D",
+			"DEFAULT" : [0.0, 0.0],
+			"MAX" : [640.0, 480.0],
+			"MIN" : [0.0, 0.0]
+		},
+		{
+			"NAME": "iColor", 
+			"TYPE" : "color", 
+			"DEFAULT" : [
+				0.9, 
+				0.6, 
+				0.0, 
+				1.0
+			]
+		}
+	],
+}
+*/
+// https://www.shadertoy.com/view/lsycDG
+
+// @lsdlive
+
+// Doodling session for live-coding or sketching new ideas.
+// Thanks to iq, mercury, lj, shane, shau, aiekick, balkhan
+// & all shadertoyers.
+// Greets to all the shader showdown paris gang.
+
+
+mat2 r2d(float a) {
+	float c = cos(a), s = sin(a);
+	return mat2(c, s, -s, c);
+}
+
+float rep(float p, float m) {
+	return mod(p - m*.5, m) - m*.5;
+}
+
+vec3 rep(vec3 p, float m) {
+	return mod(p - m*.5, m) - m*.5;
+}
+
+float cmin(float a, float b, float k) {
+	return min(min(a, b), (a - k + b) * sqrt(.5));
+}
+
+float stmin(float a, float b, float k, float n) {
+	float s = k / n;
+	float u = b - k;
+	return min(min(a, b), .5 * (u + a + abs((mod(u - a + s, 2. * s)) - s)));
+}
+
+float length8(vec2 p) {
+	vec2 q = p*p*p*p*p*p*p*p;
+	return pow(q.x + q.y, 1. / 8.);
+}
+
+float torus82(vec3 p, vec2 d) {
+	vec2 q = vec2(length(p.xz) - d.x, p.y);
+	return length8(q) - d.y;
+}
+
+float path(float t) {
+	return cos(t*.1)*2.;
+}
+
+float g = 0.;
+float de(vec3 p) {
+
+	p.x -= path(p.z);
+    
+    vec3 q = p;
+    q.x += sin(q.z*.2)*4.;
+    q.y += cos(q.z*.3)*4.;
+    q += TIME*2.;
+    q.yz += sin(TIME*.2)*4.;
+    q = rep(q, 1.);
+    float s1 = length(q) - .01 + sin(TIME*30.)*.004;
+
+	p.z = rep(p.z, 3.);
+
+	float d = torus82(p.xzy, vec2(1., .1));
+	float pl = p.y + 2.4 + p.y;//*texture(iChannel1, p.xz*.1).r*1.
+    float pl2 = p.y + .7;
+	d = min(d, pl-d*.9);
+    d = cmin(d, pl2, .1);
+
+	p.x = abs(p.x) - 1.;
+	float cyl = length(p.xy) - .05;
+	d = stmin(d, cyl, .1, 4.);
+   
+    d = min(d, s1);
+
+	g += .015 / (.01 + d*d);
+	return d;
+}
+void main(void)
+{  		
+    vec2 uv = (gl_FragCoord.xy - .5*RENDERSIZE.xy) / -RENDERSIZE.y*iZoom;
+
+    float dt = TIME * 6.;
+    vec3 ro = vec3(0, 0, -3. + dt);
+	vec3 ta = vec3(0, 0, dt);
+	ro.x += path(ro.z);
+	ta.x += path(ta.z);
+
+	vec3 fwd = normalize(ta - ro);
+	vec3 left = cross(vec3(0, 1, 0), fwd);
+	vec3 up = cross(fwd, left);
+	vec3 rd = normalize(fwd + uv.x*left + uv.y*up);
+
+	rd.xy *= r2d(sin(-ro.x / 3.14)*.4);
+
+	vec3 p;
+	float t = 0., ri;
+	for (float i = 0.; i < 1.; i += .01) {
+		ri = i;
+		p = ro + rd*t;
+		float d = de(p);
+		if (d < .001) break;
+		t += d*.3;
+	}
+
+	vec3 bg = vec3(.2, .1, .2);
+	vec3 col = bg;
+	col = mix(vec3(.4, .52, .6)*1.5, bg,  uv.x*uv.y*uv.y+ri);
+    col += g*.01;
+    col.b += sin(p.z*.1)*.1;
+	col = mix(col, bg, 1. - exp(-.01*t*t));
+    
+	fragColor = vec4(col, 1.);
+}
