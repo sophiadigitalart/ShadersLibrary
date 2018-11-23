b06c4e7e14394049c4e1a5aef8ec8c49f2830e1c
brucelane <bruce@batchass.fr>
2018-11-23 11:45:23 +0100

    vec4

--- TinyDataPath.fs
+++ TinyDataPath.fs
@@ -54,9 +54,9 @@ void main(void)
     vec2 u=iZoom * abs(gl_FragCoord.xy/RENDERSIZE.xy-.5);
     u.x*=3.;
 
-	fragColor = mix( 
+	fragColor = vec4(mix( 
         .3-.3*pow(u.x/length(u),5.), 
         step(.5,fract(sin(dot(floor(vec2(2./u.x,iSteps/u.x*u.y)),vec2(12.13,4.47)))+TIME)),
         u.x>u.y
-    )*max(u.x,u.y)*2.;
+    )*max(u.x,u.y)*2.);
 }
